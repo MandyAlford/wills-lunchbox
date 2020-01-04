aa673638d609643959b6fd40fa39fb88ecd5af5a
Mandy Alford <mandyalford@gmail.com>
2020-01-04 12:17:12 -0700

    submit lunchbox tests

--- lunchbox.js
+++ lunchbox.js
@@ -1,5 +1,23 @@
-class LunchBox {
+var Snack = require('./snack');
 
+class LunchBox {
+  constructor(boxInstance){
+    this.owner = boxInstance.owner;
+    this.material = boxInstance.madeOf;
+    this.shape = boxInstance.shape;
+    this.color = boxInstance.color;
+    this.handle = true;
+    this.snacks = [];
+  }
+  acquireSnack(snack){
+    this.snacks.push(snack);
+    snack.isInLunchBox = true;
+  }
+  findHealthySnacks(){
+    return this.snacks.filter(function(snack){
+      return snack.type.includes('Fruit');
+    });
+  }
 }
 
 module.exports = LunchBox;
